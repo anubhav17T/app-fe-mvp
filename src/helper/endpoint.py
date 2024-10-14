97f28db34c73e440cf537518d1c9e22426950955
Anubhav Tyagi <anubhav@Anubhavs-MacBook-Pro.local>
2024-10-14 20:37:59 +0530

    changed source file

--- src/helper/endpoint.py
+++ src/helper/endpoint.py
@@ -1,33 +1,33 @@
-# LOGIN_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/login"
-# REGISTER_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/register"
-# INPROGRESS_TRAINING_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/inprogress/{}"
-# IMAGE_UPLOAD_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/image-upload"
-# CHECK_TRAINING_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/training-job-name/{}/{}"
-# START_TRAINING = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/start-training"
-# FETCH_TRAINING = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/combined-training/{}"
+LOGIN_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/login"
+REGISTER_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/register"
+INPROGRESS_TRAINING_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/inprogress/{}"
+IMAGE_UPLOAD_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/image-upload"
+CHECK_TRAINING_ENDPOINT = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/training-job-name/{}/{}"
+START_TRAINING = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/start-training"
+FETCH_TRAINING = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/combined-training/{}"
+
+COMPLETED_TRAINING_JOBS = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/training-jobs/{}/COMPLETED"
+
+
+USER_PROFILE = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/profile"
+
+USER_CHANGE_PASSWORD = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/change-password"
 #
-# COMPLETED_TRAINING_JOBS = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/training/training-jobs/{}/COMPLETED"
-#
-#
-# USER_PROFILE = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/profile"
-#
-# USER_CHANGE_PASSWORD = "http://af18c1ae21c8a449d973b300b323f120-1681068879.ap-south-1.elb.amazonaws.com/api/v1/user/change-password"
-# #
 
 
 
 
-LOGIN_ENDPOINT = "http://localhost:8000/api/v1/user/login"
-REGISTER_ENDPOINT = "http://localhost:8000/api/v1/user/register"
-INPROGRESS_TRAINING_ENDPOINT = "http://localhost:8000/api/v1/user/training/inprogress/{}"
-IMAGE_UPLOAD_ENDPOINT = "http://localhost:8000/api/v1/user/training/image-upload"
-CHECK_TRAINING_ENDPOINT = "http://localhost:8000/api/v1/user/training/training-job-name/{}/{}"
-START_TRAINING = "http://localhost:8000/api/v1/user/training/start-training"
-FETCH_TRAINING = "http://localhost:8000/api/v1/user/training/combined-training/{}"
-COMPLETED_TRAINING_JOBS = "http://localhost:8000/api/v1/user/training/training-jobs/{}/COMPLETED"
-USER_PROFILE = "http://localhost:8000/api/v1/user/profile"
-USER_CHANGE_PASSWORD = "http://localhost:8000/api/v1/user/change-password"
-LOCALHOST = "localhost:8000"
+# LOGIN_ENDPOINT = "http://localhost:8000/api/v1/user/login"
+# REGISTER_ENDPOINT = "http://localhost:8000/api/v1/user/register"
+# INPROGRESS_TRAINING_ENDPOINT = "http://localhost:8000/api/v1/user/training/inprogress/{}"
+# IMAGE_UPLOAD_ENDPOINT = "http://localhost:8000/api/v1/user/training/image-upload"
+# CHECK_TRAINING_ENDPOINT = "http://localhost:8000/api/v1/user/training/training-job-name/{}/{}"
+# START_TRAINING = "http://localhost:8000/api/v1/user/training/start-training"
+# FETCH_TRAINING = "http://localhost:8000/api/v1/user/training/combined-training/{}"
+# COMPLETED_TRAINING_JOBS = "http://localhost:8000/api/v1/user/training/training-jobs/{}/COMPLETED"
+# USER_PROFILE = "http://localhost:8000/api/v1/user/profile"
+# USER_CHANGE_PASSWORD = "http://localhost:8000/api/v1/user/change-password"
+# LOCALHOST = "localhost:8000"
 
 
 
